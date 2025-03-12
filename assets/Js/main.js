202a62f7a13e805f461415657f60ac9dd76a0f1c
PRASANTH M <151944542+gitmprasanth@users.noreply.github.com>
2025-03-12 12:46:15 +0530

    get day based on date

--- assets/Js/main.js
+++ assets/Js/main.js
@@ -1,19 +1,30 @@
  const dateFormEl=document.querySelector('#dateForm')
 const dayImgEl=document.querySelector('#dayImg')
 //  https://srirangaminfo.com/cal/2025/big/0103.jpg 
-const myDate=new Date();
-const result=myDate.toLocaleDateString("en-IN",{
-    day:"2-digit",
-    month:"2-digit",
+
+dateFormEl.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const inputDate=e.target[0].value
+   processDate(inputDate);
 })
 
-
-const formatDate=result.replaceAll("/","");
-
-const formatedurl=`https://srirangaminfo.com/cal/2025/big/${formatDate}.jpg`
-
-dayImgEl.src=formatedurl;
-dayImgEl.alt=formatDate
-dayImgEl.classList.remove('hidden')
-
+function processDate(date){
+     const myDate=date?new Date(date):new Date();
+    const result=myDate.toLocaleDateString("en-IN",{
+        day:"2-digit",
+        month:"2-digit",
+    })
+    
+    const formatDate=result.replaceAll("/","");
+    console.log(myDate.getFullYear());
+    console.log(formatDate);
+    
+    
+    const formatedurl=`https://srirangaminfo.com/cal/${myDate.getFullYear()}/big/${formatDate}.jpg`
+    
+    dayImgEl.src=formatedurl;
+    dayImgEl.alt=formatDate
+    dayImgEl.classList.remove('hidden')  
+    
+};
 
